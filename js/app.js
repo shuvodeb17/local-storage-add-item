9d5b420e1912434d75930223f0f2e2133ebc443f
shuvodeb17 <shuvodeb17014@gmail.com>
2022-10-30 03:42:50 -0700

    add item UI

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ const addItemBtn = () => {
     const addBarText = addBar.value;
     console.log(addBarText);
     addBar.value = '';
+    displayUi(addBarText);
+}
+// display UI
+const displayUi = productName => {
+    const ul = document.getElementById('add');
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <li>${productName}</li>
+    `
+    ul.appendChild(li);
 }
